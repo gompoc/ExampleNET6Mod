0bcc88468209365c6aec88b536856ee1c11d4f4f
gompoc <91314780+gompoc@users.noreply.github.com>
2022-04-14 21:37:34 +0100

    Add solution and project

---
+++ ExampleMod/Main.cs
@@ -0,0 +1,17 @@
+﻿using MelonLoader;
+using UnityEngine;
+
+[assembly:MelonInfo(typeof(ExampleMod.Main), "ExampleMod", "1.0.0", "gompo")]
+[assembly:MelonGame(developer: null, name: null)]
+
+namespace ExampleMod;
+
+public class Main : MelonMod
+{
+    public override void OnUpdate() {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            LoggerInstance.Msg("Pressed the P key!");
+        }
+    }
+}
